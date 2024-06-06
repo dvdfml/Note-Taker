ef6f06ecb3cd9c37bc33c54602736942e663f427
dvdfml <dvdfermiranda@yahoo.com.mx>
2024-06-05 21:27:31 -0600

    Added GET and POST routes to retrieve and create notes. Added id helper.

--- routes/notes.js
+++ routes/notes.js
@@ -1,3 +1,36 @@
 const notes = require('express').Router();
 
+// Helper function to generate unique id's
+const uuid = require('../helpers/uuid');
+
+// Helper functions for reading and writing to the JSON file
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+
+// This API route is a GET Route for retrieving all the notes
+notes.get('/', (req, res) => {
+  console.info(`${req.method} request received for notes`);
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+
+// This API route is a POST Route for a new note
+notes.post('/', (req, res) => {
+    console.info(`${req.method} request received to add a note`);
+  
+    const { title, text } = req.body;
+  
+    if (req.body) {
+      const newNote = {
+        id: uuid(),
+        title,
+        text,
+      };
+  
+      readAndAppend(newNote, './db/db.json');
+      res.json(`Note added successfully 🚀`);
+    } else {
+      res.error('Error in adding note');
+    }
+  });
+
 module.exports = notes;
